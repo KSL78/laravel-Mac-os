3e494855c7c544c775ddfa3ba302610abda4b990
KSL78 <acc8041@gmail.com>
2024-04-12 04:18:45 +0900

    Update web.php

--- mtv1/routes/web.php
+++ mtv1/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users/{name}', function ($name) {
+    return 'welcome, '. $name;
+});
