0988e862e892582a098c747a00b0f48000e4509a
KSL78 <acc8041@gmail.com>
2024-04-21 22:53:58 +0900

    Update web.php

--- mtv1/routes/web.php
+++ mtv1/routes/web.php
@@ -6,6 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users/{name}', function ($name) {
+Route::get('/users/{name?}', function ($name = NULL) {
     return 'welcome, '. $name;
 });
