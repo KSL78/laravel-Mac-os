4c20fbe6870b8ad7d6d31238252121b8c0c494f1
KSL78 <acc8041@gmail.com>
2024-04-21 23:10:22 +0900

    Update web.php

--- mtv1/routes/web.php
+++ mtv1/routes/web.php
@@ -8,4 +8,8 @@ Route::get('/', function () {
 
 Route::get('/users/{name?}', function ($name = NULL) {
     return 'welcome, '. $name;
-});
+})->where('name','[a-zA-Z0-9]+');
+
+Route::get('/product/{id?}', function ($id = NULL) {
+    return '제품 id:'. $id;
+})->where('id','[0-9]+');
