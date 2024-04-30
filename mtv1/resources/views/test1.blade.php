dd4d8f0eab636cddee62a4fc3a159cae9a0badff
KSL78 <acc8041@gmail.com>
2024-04-30 21:48:14 +0900

    Update test1.blade.php

--- mtv1/resources/views/test1.blade.php
+++ mtv1/resources/views/test1.blade.php
@@ -9,4 +9,11 @@
     숫자가 증가합니다. {{$i}} <br>
 @endfor
 @include ('inc') <br>
-@include ('incf.incfs')
+@include('incf.incfs')
+@includeIf('incf.incf')<br>
+@php
+    $a = 0;
+    $a++;
+    echo '<p>a의 값은'.$a.'입니다</p>';
+@endphp
+{{--주석입니다.--}}
